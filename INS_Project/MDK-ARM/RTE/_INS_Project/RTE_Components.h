eeae0548418d7771f941f1e5fdfc88a7ccea6704
zhenxing <lizhenxing057@gmail.com>
2024-05-11 16:39:06 +0800

    start project

---
+++ INS_Project/MDK-ARM/RTE/_INS_Project/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'INS_Project' 
+ * Target:  'INS_Project' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
